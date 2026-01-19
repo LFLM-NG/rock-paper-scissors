62ab2eeae24dd67639bbf8a82d1a3b21bc2282fe
LFLM-NG <gergoryheffley@gmail.com>
2026-01-19 16:06:28 +0700

    Add function for computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 100);
+    if (computerChoice < 33) {
+        return "Rock";
+    } else if (computerChoice < 66){
+        return "Paper";
+    }
+    return "Scissors";
+}
+console.log(getComputerChoice());
